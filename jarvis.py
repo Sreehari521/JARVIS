feb2a208c555cf05d0abab14fdf282336989b8d2
Sreehari521 <82920449+Sreehari521@users.noreply.github.com>
2021-10-08 12:46:40 +0530

    Add files via upload

--- jarvis.py
+++ jarvis.py
@@ -6,6 +6,9 @@ import wikipedia
 import pyjokes
 import sys
 import psutil
+import webbrowser
+import requests
+import os
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -74,16 +77,62 @@ def run():
     elif 'good night' in command:
         talk("Good bye sir, see you tomorrow")
 
-    elif 'turn off' in command:
+    elif 'turn off' in command or 'shut up' in command or 'stop' in command:
         talk("Okay sir")
         sys.exit()
 
-    elif 'battery' or 'battery percentage' in command:
+    elif 'battery' in command:
         battery = psutil.sensors_battery()
         percentage = battery.percent
         talk(f"Sir, your system have {percentage} percent battery")
 
+    elif 'open google' in command:
+        talk("Sure Sir")
+        talk("Opening Google")
+        webbrowser.open("http://www.google.com")
+        talk("Job Done Sir")
+
+    elif 'open youtube' in command:
+        talk("Sure Sir")
+        talk("Opening YouTube")
+        webbrowser.open("http://www.youtube.com")
+        talk("Job Done Sir")
+
+    elif 'open scratch' in command:
+        talk("Sure Sir")
+        talk("Opening Scratch")
+        webbrowser.open("http://scratch.mit.edu")
+        talk("Job Done Sir")
+
+    elif 'open notepad' in command:
+        talk("Sure Sir")
+        talk("Opening Notepad")
+        os.startfile("C:/Windows/System32/notepad.exe")
+        talk("Job Done Sir")
+
+    elif 'open command prompt' in command or 'open cmd' in command:
+        talk("Sure Sir")
+        talk("Opening Command Prompt")
+        os.system("start cmd")
+        talk("Job Done Sir")
+
+    elif 'where are we' in command or 'where am i' in command or 'where i am' in command or 'where we are' in command:
+        talk("I am looking that Sir, please wait some seconds")
+        try:
+            ipAdr = requests.get("https://api.ipify.org").text
+            print(ipAdr)
+            url = "https://get.geojs.io/v1/ip/geo/" + ipAdr + '.json'
+            geo_requests = requests.get(url)
+            geo_data = geo_requests.json()
+            city = geo_data["city"]
+            country = geo_data["country"]
+            print(f'Sir, I am not sure but I think that we are in the {city} city of {country}')
+            talk(f'Sir, I am not sure but I think that we are in the {city} city of {country}')
+        except Exception as e:
+            talk("Sorry Sir, Because of some network issues or some other issues I can't find where we are.")
+
     else:
+        talk("I don't understand that command")
         run()
 
 while True:
